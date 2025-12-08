cb8f84da87d5e7c1c57ec5b830a914d72a4871c1
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2025-12-08 16:06:47 -0500

    media

--- lima_visual/settings.py
+++ lima_visual/settings.py
@@ -147,8 +147,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'lima_visual/static'),)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/media'
+
 
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
