1739945c10958c4e4caee9df16fee6e92ac88ee4
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2025-12-09 12:33:18 -0500

    prueba11

--- lima_visual/settings.py
+++ lima_visual/settings.py
@@ -158,7 +158,7 @@ LOGIN_URL = 'inicial'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'lima_visual/static'),)
 
