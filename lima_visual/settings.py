8b551f9a252bb47a90f359e8184a55d5756292dd
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2025-12-09 13:30:58 -0500

    nueevo

--- lima_visual/settings.py
+++ lima_visual/settings.py
@@ -159,6 +159,11 @@ LOGIN_URL = 'inicial'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'lima_visual/static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/media'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
@@ -167,9 +172,9 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-LOGIN_URL = 'inicial'
+# Default primary key field type
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'lima_visual/static'),)
+
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Default primary key field type
