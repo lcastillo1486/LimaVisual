049ecf94b16efdde105081b9a3148b898a9f941e
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2025-12-09 11:33:37 -0500

    prueba4

--- lima_visual/settings.py
+++ lima_visual/settings.py
@@ -168,7 +168,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
