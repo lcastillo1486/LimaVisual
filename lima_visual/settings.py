63d43a183b19c383ebc9374c295241cde99ecfbf
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2025-12-09 11:42:24 -0500

    prueba5

--- lima_visual/settings.py
+++ lima_visual/settings.py
@@ -163,7 +163,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'lima_visual', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
